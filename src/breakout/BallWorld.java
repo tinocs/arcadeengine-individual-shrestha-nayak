5f906daf4438e92e872db6730bc6ee542bf847d7
shrestha-nayak <shrestha.nayak2020@gmail.com>
2026-04-26 21:48:54 -0700

    Added Title Screen

--- src/breakout/BallWorld.java
+++ src/breakout/BallWorld.java
@@ -1,54 +1,101 @@
 package breakout;
 
+import java.io.InputStream;
+import java.util.Scanner;
+
 import engine.World;
 import javafx.event.EventHandler;
 import javafx.scene.input.MouseEvent;
 
-public class BallWorld extends World{
-	
-	private Paddle paddle;
+public class BallWorld extends World {
+
+    private Paddle paddle;
     private Ball ball;
     private Score score;
-	
-	public BallWorld() {
-		setPrefSize(800, 600);
-	}
-	
-	@Override
-	public void onDimensionsInitialized() {
-		ball = new Ball();
-		add(ball);
-		ball.setX(getWidth() / 2);
-		ball.setY(getHeight() / 2);
-		
-		paddle = new Paddle();
-		add(paddle);
-		paddle.setX(getWidth() / 2);
-		paddle.setY(3 * getHeight() / 4);
-		
-		setOnMouseMoved(new EventHandler<MouseEvent>() {
-			@Override
-			public void handle(MouseEvent event) {
-				paddle.setX(event.getX() - paddle.getWidth() / 2);
-			}});
-		
-		//Test Brick Logic
-		/*Brick brick = new Brick();
-		add(brick);
-		brick.setX(getWidth() / 2);
-		brick.setY(100);*/
-		
-		score = new Score();
+    private int level;
+
+    public BallWorld() {
+        setPrefSize(800, 600);
+        level = 1;
+    }
+
+    @Override
+    public void onDimensionsInitialized() {
+        ball = new Ball();
+        add(ball);
+        ball.setX(getWidth() / 2);
+        ball.setY(getHeight() / 2);
+
+        paddle = new Paddle();
+        add(paddle);
+        paddle.setX(getWidth() / 2);
+        paddle.setY(3 * getHeight() / 4);
+
+        setOnMouseMoved(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                paddle.setX(event.getX() - paddle.getWidth() / 2);
+            }
+        });
+
+        score = new Score();
         score.setX(getWidth() / 12);
         score.setY(getHeight() / 10);
         getChildren().add(score);
+
+        loadLevel(level);
+    }
+
+    public void loadLevel(int level) {
+        String fileName = "breakoutresources/level" + level + ".txt";
+
+        InputStream input = getClass().getClassLoader().getResourceAsStream(fileName);
+        if (input == null) {
+            Breakout.showTitleScreen();
+            return;
+        }
+
+        Scanner scanner = new Scanner(input);
+
+        int rows = scanner.nextInt();
+        int cols = scanner.nextInt();
+        scanner.nextLine();
+
+        Brick sample = new Brick();
+        double brickWidth = sample.getImage().getWidth();
+        double brickHeight = sample.getImage().getHeight();
+
+        for (int r = 0; r < rows; r++) {
+            String line = scanner.nextLine();
+
+            for (int c = 0; c < cols; c++) {
+                if (line.charAt(c) != '0') {
+                    Brick brick = new Brick();
+                    add(brick);
+                    brick.setX(c * brickWidth + 90);
+                    brick.setY(r * brickHeight + 100);
+                }
+            }
+        }
+
+        scanner.close();
     }
 
     public Score getScore() {
         return score;
     }
 
-	@Override
-	public void act(long now) {
-	}
+    @Override
+    public void act(long now) {
+        if (getObjects(Brick.class).isEmpty()) {
+            level++;
+
+            if (level <= 2) {
+                loadLevel(level);
+            } else {
+                stop();
+                Breakout.showTitleScreen();
+            }
+        }
+    }
 }
--- src/breakout/Breakout.java
+++ src/breakout/Breakout.java
@@ -2,26 +2,48 @@ package breakout;
 
 import javafx.application.Application;
 import javafx.event.EventHandler;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.*;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class Breakout extends Application{
+	public static Stage stage;
+	
 	public static void main(String[] args) {
 		launch(args);
 	}
 	
 	@Override
-	public void start(Stage stage) throws Exception {
-		stage.setTitle("Breakout");
-		BorderPane root = new BorderPane();
-		BallWorld bw = new BallWorld();
-		root.setCenter(bw);
-		Scene scene = new Scene(root);
-		bw.start();
-		stage.setScene(scene);
-		stage.show();
-	}
+    public void start(Stage primaryStage) {
+        stage = primaryStage;
+        stage.setTitle("Breakout");
+        showTitleScreen();
+        stage.show();
+    }
 
+    public static void showTitleScreen() {
+        Text title = new Text("Breakout");
+        title.setFont(new Font(48));
+
+        Button playButton = new Button("Play");
+        playButton.setOnAction(e -> {
+            BallWorld world = new BallWorld();
+            Scene gameScene = new Scene(world, 800, 600);
+            stage.setScene(gameScene);
+            world.start();
+        });
+
+        VBox layout = new VBox(30);
+        layout.setAlignment(Pos.CENTER);
+        layout.getChildren().addAll(title, playButton);
+
+        Scene titleScene = new Scene(layout, 800, 600);
+        stage.setScene(titleScene);
+    }
 }
+
